e1e765e518ed91fb9eb63eeef07815bca38fb05d
Demian <admin@hellofable.com>
2025-03-17 17:03:49 -0700

    whee

--- src/components/Scripts/ScriptsSidebar/ScriptsSidebar.svelte
+++ src/components/Scripts/ScriptsSidebar/ScriptsSidebar.svelte
@@ -1,32 +1,35 @@
 <script>
-	import { _app } from '$lib';
-	import BottomButtonBar from './BottomButtonBar/BottomButtonBar.svelte';
+  import { _app } from "$lib";
+  import BottomButtonBar from "./BottomButtonBar/BottomButtonBar.svelte";
 
-	import AccountCard from '/src/components/User/AccountCard/AccountCard.svelte';
-	import SubscriptionCard from '/src/components/User/SubscriptionCard/SubscriptionCard.svelte';
-	import Vault from '/src/components/Vault/Vault.svelte';
+  import AccountCard from "/src/components/User/AccountCard/AccountCard.svelte";
+  import SubscriptionCard from "/src/components/User/SubscriptionCard/SubscriptionCard.svelte";
+  import Vault from "/src/components/Vault/Vault.svelte";
 </script>
 
-<div class="d-flex flex-column scripts-sidebar h-100" class:d-none={!$_app.scriptsSidebar.show}>
-	<div class="flex-grow-1 overflow-y-auto scroll">
-		{#if $_app.scriptsSidebar.bottomButtons.selected === 'Vault'}
-			<Vault />
-		{/if}
-		{#if $_app.scriptsSidebar.bottomButtons.selected === 'Account'}
-			<div class="p-2">
-				<AccountCard />
-				<SubscriptionCard />
-			</div>
-		{/if}
-	</div>
+<div
+  class="d-flex flex-column scripts-sidebar h-100"
+  class:d-none={!$_app.scriptsSidebar.show}
+>
+  <div class="flex-grow-1 overflow-y-auto scroll">
+    <Vault />
 
-	<BottomButtonBar />
+    <div
+      class:d-none={$_app.scriptsSidebar.bottomButtons.selected !== "Account"}
+      class="p-2"
+    >
+      <AccountCard />
+      <SubscriptionCard />
+    </div>
+  </div>
+
+  <BottomButtonBar />
 </div>
 
 <style>
-	.scripts-sidebar {
-		height: 100%;
-		width: 300px;
-		flex: 0 0 300px; /* Prevents flexbox from resizing it */
-	}
+  .scripts-sidebar {
+    height: 100%;
+    width: 300px;
+    flex: 0 0 300px; /* Prevents flexbox from resizing it */
+  }
 </style>
--- src/components/Vault/Vault.svelte
+++ src/components/Vault/Vault.svelte
@@ -1,25 +1,28 @@
 <script>
-	import { _scripts } from '$lib';
-	import VaultOptions from './VaultOptions/VaultOptions.svelte';
+  import { _scripts, _app } from "$lib";
+  import VaultOptions from "./VaultOptions/VaultOptions.svelte";
 
-	let scriptsSorted = [];
+  let scriptsSorted = [];
 
-	$: if ($_scripts) {
-		scriptsSorted = $_scripts.sort((a, b) => b.scriptNumber - a.scriptNumber);
-	}
+  $: if ($_scripts) {
+    scriptsSorted = $_scripts.sort((a, b) => b.scriptNumber - a.scriptNumber);
+  }
 
-	// import VaultOptions from './VaultOptions/VaultOptions.svelte';
-	import VaultCard from './VaultCard/VaultCard.svelte';
+  // import VaultOptions from './VaultOptions/VaultOptions.svelte';
+  import VaultCard from "./VaultCard/VaultCard.svelte";
 </script>
 
-<div class="d-flex flex-column h-100">
-	<VaultOptions />
-	<div class="flex-grow-1 overflow-auto pt-2 ps-2 pe-0">
-		<p class="alert alert-secondary text-secondary d-none">
-			Vault is empty. Your documents will be backed up here when enabled.
-		</p>
-		{#each scriptsSorted as script (script.id)}
-			<div class="mb-1"><VaultCard {script} /></div>
-		{/each}
-	</div>
+<div
+  class:d-none={$_app.scriptsSidebar.bottomButtons.selected !== "Vault"}
+  class="d-flex flex-column h-100"
+>
+  <VaultOptions />
+  <div class="flex-grow-1 overflow-auto pt-2 ps-2 pe-0">
+    <p class="alert alert-secondary text-secondary d-none">
+      Vault is empty. Your documents will be backed up here when enabled.
+    </p>
+    {#each scriptsSorted as script (script.id)}
+      <div class="mb-1"><VaultCard {script} /></div>
+    {/each}
+  </div>
 </div>
