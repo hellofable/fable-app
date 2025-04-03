37050f86e5377c97227be1a02666d0f377eeca0d
Demian <admin@hellofable.com>
2025-04-03 13:42:44 -0700

    whee

--- src/components/Toolbar/buttons/FullScreenButton.svelte
+++ src/components/Toolbar/buttons/FullScreenButton.svelte
@@ -1,21 +1,34 @@
 <script>
-	import { onMount } from 'svelte';
-	let isFull = false;
-	export let roundedClass = '';
+  import { onMount } from "svelte";
+  let isFull = false;
+  export let roundedClass = "";
 
-	onMount(() => {
-		document.addEventListener('fullscreenchange', () => {
-			isFull = document.fullscreenElement ? true : false;
-		});
-	});
+  onMount(() => {
+    document.addEventListener("fullscreenchange", () => {
+      isFull = document.fullscreenElement ? true : false;
+    });
+  });
 
-	function fullscreen() {
-		document.fullscreenElement
-			? document.exitFullscreen()
-			: document.documentElement.requestFullscreen();
-	}
+  function fullscreen() {
+    document.fullscreenElement
+      ? document.exitFullscreen()
+      : document.documentElement.requestFullscreen();
+  }
+
+  let hasFullscreen = false;
+  onMount(() => {
+    if (document.documentElement.requestFullscreen) {
+      hasFullscreen = true;
+    }
+  });
 </script>
 
-<button class:selected={isFull} on:click={fullscreen} class="btn {roundedClass}">
-	<i class="bi bi-fullscreen"></i>
-</button>
+{#if hasFullscreen}
+  <button
+    class:selected={isFull}
+    on:click={fullscreen}
+    class="btn {roundedClass}"
+  >
+    <i class="bi bi-fullscreen"></i>
+  </button>
+{/if}
