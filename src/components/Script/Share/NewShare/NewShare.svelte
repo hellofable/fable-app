05108071362414484c1a9727cde6629a271dae64
Demian <admin@hellofable.com>
2025-04-10 16:17:38 -0700

    whee

--- src/components/Script/Share/NewShare/NewShare.svelte
+++ src/components/Script/Share/NewShare/NewShare.svelte
@@ -1,54 +1,59 @@
 <script>
-	import EasyForm from '/src/components/Other/EasyForm.svelte';
-	import ToggleShare from './ToggleShare.svelte';
-	let show = false;
-	import { _script, pb, toast } from '$lib';
+  import EasyForm from "/src/components/Other/EasyForm.svelte";
+  import ToggleShare from "./ToggleShare.svelte";
+  let show = false;
+  import { _script, pb, toast } from "$lib";
 
-	async function handleSubmit(data) {
-		props.disabled = true;
-		const { email } = data;
-		const scriptId = $_script.id;
-		const res = await pb.db.scripts.addShare({ scriptId, email });
-		props.disabled = false;
+  async function handleSubmit(data) {
+    console.log("add shared");
 
-		if (res.success) {
-			// show = false;
-			toast({ type: 'success', message: 'Share added successfully' });
-			return res;
-		}
-		if (!res.success) {
-			toast({ type: 'error', message: res.message });
-			return res;
-		}
-	}
+    props.disabled = true;
+    const { email } = data;
+    const scriptId = $_script.id;
 
-	let props = {
-		submitTitle: 'Add Share',
-		submitClass: 'btn-primary btn-sm',
-		disabled: false
-	};
-	let fields = [
-		{
-			name: 'email',
-			type: 'email',
-			placeholder: 'Enter email of share recipient',
-			required: true,
-			showLabel: false
-		}
-	];
+    const res = await pb.db.scripts.addShare({ scriptId, email });
+    props.disabled = false;
+
+    if (res.success) {
+      // show = false;
+      toast({ type: "success", message: "Share added successfully" });
+      return res;
+    }
+    if (!res.success) {
+      toast({ type: "error", message: res.message });
+      return res;
+    }
+  }
+
+  let props = {
+    submitTitle: "Add Share",
+    submitClass: "btn-primary btn-sm",
+    disabled: false,
+  };
+  let fields = [
+    {
+      name: "email",
+      type: "email",
+      placeholder: "Enter email of share recipient",
+      required: true,
+      showLabel: false,
+    },
+  ];
 </script>
 
 <div class="addNewShare rounded p-3">
-	<div class="d-flex">
-		<div class="me-2"><ToggleShare bind:show /></div>
-	</div>
-	{#if show}
-		<div class="mt-3"><EasyForm bind:fields bind:props onSubmit={handleSubmit} /></div>
-	{/if}
+  <div class="d-flex">
+    <div class="me-2"><ToggleShare bind:show /></div>
+  </div>
+  {#if show}
+    <div class="mt-3">
+      <EasyForm bind:fields bind:props onSubmit={handleSubmit} />
+    </div>
+  {/if}
 </div>
 
 <style>
-	.addNewShare {
-		background: rgba(129, 129, 129, 0.1);
-	}
+  .addNewShare {
+    background: rgba(129, 129, 129, 0.1);
+  }
 </style>
--- src/components/Script/Share/ShareRow.svelte
+++ src/components/Script/Share/ShareRow.svelte
@@ -1,41 +1,41 @@
 <script>
-	import { _script, _user, pb } from '$lib';
-	import { onMount } from 'svelte';
-	export let share;
-	import Leave from './Buttons/Leave.svelte';
-	import RemoveShare from './Buttons/RemoveShare.svelte';
+  import { _script, _user, pb } from "$lib";
+  import { onMount } from "svelte";
+  export let share;
+  import Leave from "./Buttons/Leave.svelte";
+  import RemoveShare from "./Buttons/RemoveShare.svelte";
 
-	let sharedWith;
-	let isLoading = true;
-	let sharedBy = '';
+  let sharedWith;
+  let isLoading = true;
+  let sharedBy = "";
 
-	console.log(share);
+  console.log(share);
 
-	onMount(async () => {
-		sharedBy = await pb.collection('user_info').getOne(share.sharedWith);
-		sharedWith = sharedBy;
-		isLoading = false;
-	});
+  onMount(async () => {
+    sharedBy = await pb.collection("user_info").getOne(share.sharedWith);
+    sharedWith = sharedBy;
+    isLoading = false;
+  });
 </script>
 
 <tr class:d-none={isLoading}>
-	<td>
-		{sharedWith?.name}
-		{#if $_script.ownerId !== $_user.id}
-			<small> (you)</small>
-		{/if} <br />
-		<div class="small text-secondary">{sharedWith?.email}</div>
-	</td>
+  <td>
+    {sharedWith?.name}
+    {#if $_script.ownerId !== $_user.id}
+      <small> (you)</small>
+    {/if} <br />
+    <div class="small text-secondary">{sharedWith?.email}</div>
+  </td>
 
-	{#if sharedWith?.email !== $_user.email}
-		<td class="text-end align-middle">
-			<RemoveShare {sharedWith} />
-		</td>
-	{/if}
+  {#if sharedWith?.email !== $_user.email}
+    <td class="text-end align-middle">
+      <RemoveShare {sharedWith} />
+    </td>
+  {/if}
 
-	{#if $_script.ownerId !== $_user.id}
-		<td class="text-end align-middle">
-			<Leave />
-		</td>
-	{/if}
+  {#if $_script.ownerId !== $_user.id}
+    <td class="text-end align-middle">
+      <Leave />
+    </td>
+  {/if}
 </tr>
