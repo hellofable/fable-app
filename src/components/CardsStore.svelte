90910c37fd844ce7a253c0ac9d8048b2f1459772
Demian <admin@hellofable.com>
2025-04-11 08:12:49 -0700

    whee

--- src/components/CardsStore.svelte
+++ src/components/CardsStore.svelte
@@ -1,42 +1,42 @@
 <script>
-	import { _cards, _app } from '$lib';
-	let show = false;
+  import { _cards, _app } from "$lib";
+  let show = false;
 </script>
 
 <div class="store-wrapper">
-	<button
-		class="btn btn-link"
-		on:click={() => {
-			show = !show;
-		}}>Store</button
-	>
-	{#if show}
-		<div class:bg-dark={show} class:d-none={!show}>
-			{$_cards.length}
-			<pre> {JSON.stringify($_cards, null, 2)}</pre>
-		</div>
-	{/if}
+  <button
+    class="btn btn-link"
+    on:click={() => {
+      show = !show;
+    }}>Store</button
+  >
+  {#if show}
+    <div class:bg-dark={show} class:d-none={!show}>
+      {$_cards.length}
+      <pre> {JSON.stringify($_cards, null, 2)}</pre>
+    </div>
+  {/if}
 </div>
 
 <style>
-	pre {
-		white-space: pre-wrap; /* Enables wrapping */
-		word-break: break-word; /* Breaks words if they’re too long */
-		width: 80vw;
-		height: 200px;
-	}
+  pre {
+    white-space: pre-wrap; /* Enables wrapping */
+    word-break: break-word; /* Breaks words if they’re too long */
+    width: 80vw;
+    height: 200px;
+  }
 
-	.store-wrapper {
-		position: absolute;
-		z-index: 2;
-		top: 50px;
-		right: 100px;
+  .store-wrapper {
+    position: absolute;
+    z-index: 2;
+    top: 50px;
+    right: 100px;
 
-		color: white !important;
-	}
+    color: white !important;
+  }
 
-	button {
-		background: black;
-		opacity: 0.5;
-	}
+  button {
+    background: black;
+    opacity: 0.5;
+  }
 </style>
